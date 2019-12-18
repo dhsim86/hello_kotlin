1722883621e6be6672c16789b986f1e9b7b7d734
Dongho Sim <dhsim86@gmail.com>
2019-12-18 13:40:15 +0900

    Kotlin Test 18: lambda return

--- src/test/kotlin/com/dongho/dev/lecture/func/InlineFunctionTest.kt
+++ src/test/kotlin/com/dongho/dev/lecture/func/InlineFunctionTest.kt
@@ -6,6 +6,18 @@ import org.junit.jupiter.api.Test
 inline fun calculator(a: Int, b: Int, op: (Int, Int) -> Int): Int = op(a, b)
 inline fun calculato2(a: Int, b: Int, noinline op: (Int, Int) -> Int): Int = op(a, b)
 
+fun apply1(op: () -> Unit) {
+    println("start apply")
+    op()
+    println("end apply")
+}
+
+inline fun apply2(op: () -> Unit) {
+    println("start apply")
+    op()
+    println("end apply")
+}
+
 class InlineFunctionTest {
 
     @Test
@@ -44,5 +56,84 @@ class InlineFunctionTest {
         println(calculator(1, 2) {a, b -> a + b})
     }
 
+    @Test
+    @DisplayName("non-local return Test")
+    fun nonLocalReturnTest() {
+
+        data class Person(val name: String, val age: Int)
+
+        val people = listOf(Person("person1", 21), Person("person2", 22), Person("person3", 23))
+
+        """
+        start apply
+        Find!
+        """
+        apply2 {
+            for (person in people) {
+                if (person.name == "person2") {
+                    println("Find!")
+                    return
+                }
+            }
+            println("Not found")
+        }
+    }
+
+    @Test
+    @DisplayName("local return Test")
+    fun localReturnTest() {
+
+        data class Person(val name: String, val age: Int)
+
+        val people = listOf(Person("person1", 21), Person("person2", 22), Person("person3", 23))
+
+        """
+        start apply
+        Find!
+        end apply
+        """
+        apply1 {
+            for (person in people) {
+                if (person.name == "person2") {
+                    println("Find!")
+                    return@apply1
+                }
+            }
+            println("Not found")
+        }
+        /*
+        apply1 label@ {
+            for (person in people) {
+                if (person.name == "person2") {
+                    println("Find!")
+                    return@label
+                }
+            }
+            println("Not found")
+        }*/
+    }
+
+    @Test
+    @DisplayName("anonymous return Test")
+    fun anonymousReturnTest() {
+        data class Person(val name: String, val age: Int)
+
+        val people = listOf(Person("person1", 21), Person("person2", 22), Person("person3", 23))
+
+        """
+        start apply
+        Find!
+        end apply
+        """
+        apply1 (fun() {
+            for (person in people) {
+                if (person.name == "person2") {
+                    println("Find!")
+                    return
+                }
+            }
+            println("Not found")
+        })
+    }
 
 }
